55e94594963a52bd4f354a0eee62ca710eedd6f3
Patrick Kage <patrick.r.kage@gmail.com>
2023-02-09 03:45:12 +0000

    hopefully fix proxy issue

--- server.py
+++ server.py
@@ -3,12 +3,18 @@ from __future__ import division, print_function
 
 import logging
 import os
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from virtual.web import app
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger("rasterio._base").setLevel(logging.WARNING)
 LOG = logging.getLogger(__name__)
+
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 
 if __name__ == "__main__":
